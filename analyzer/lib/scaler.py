859fa11c7b8acbd51b9abeb3445df63d89050679
Dat Nguyen <datnguyen1215@gmail.com>
2023-12-13 18:38:55 -0800

    update scale angles.

--- analyzer/lib/scaler.py
+++ analyzer/lib/scaler.py
@@ -18,19 +18,23 @@ def scale(df, scaler=None):
 # we'll split the target into two groups, one for positive angles and one for negative angles
 # then we'll scale them separately
 def scale_angle(df):
-    negative_df = df.copy()
-    negative_df[negative_df > 0] = 0
+    flat_data = df.values.flatten()
 
+    # initialize scalers
     negative_scaler = MinMaxScaler(feature_range=(0, 0.5))
-    negative_df = negative_scaler.fit_transform(df)
-
-    positive_df = df.copy()
-    positive_df[positive_df < 0] = 0
-
     positive_scaler = MinMaxScaler(feature_range=(0.5, 1))
-    positive_df = positive_scaler.fit_transform(df)
 
-    df[df > 0] = positive_df
-    df[df < 0] = negative_df
+    # perform scaling
+    flat_data[flat_data < 0] = negative_scaler.fit_transform(
+        flat_data[flat_data < 0].reshape(-1, 1)
+    ).ravel()
+    flat_data[flat_data > 0] = positive_scaler.fit_transform(
+        flat_data[flat_data > 0].reshape(-1, 1)
+    ).ravel()
 
-    return [df, positive_scaler, negative_scaler]
+    # reshape data back to original shape
+    scaled_df = pd.DataFrame(
+        flat_data.reshape(df.shape), columns=df.columns, index=df.index
+    )
+
+    return [scaled_df, positive_scaler, negative_scaler]
