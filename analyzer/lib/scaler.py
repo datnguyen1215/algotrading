013b1c8e847cd39f39876291669d7e3d31cfee56
Dat Nguyen <datnguyen1215@gmail.com>
2023-12-14 03:41:19 -0800

    fix scaler issues.

--- analyzer/lib/scaler.py
+++ analyzer/lib/scaler.py
@@ -18,23 +18,25 @@ def scale(df, scaler=None):
 # we'll split the target into two groups, one for positive angles and one for negative angles
 # then we'll scale them separately
 def scale_angle(df):
-    flat_data = df.values.flatten()
+    df = df.copy()
+
+    # create df copy
+    negative_df = df.copy()
+    positive_df = df.copy()
+
+    # set negative values to 0
+    negative_df[negative_df > 0] = 0
+    positive_df[positive_df < 0] = 0
 
     # initialize scalers
     negative_scaler = MinMaxScaler(feature_range=(0, 0.5))
     positive_scaler = MinMaxScaler(feature_range=(0.5, 1))
 
-    # perform scaling
-    flat_data[flat_data < 0] = negative_scaler.fit_transform(
-        flat_data[flat_data < 0].reshape(-1, 1)
-    ).ravel()
-    flat_data[flat_data > 0] = positive_scaler.fit_transform(
-        flat_data[flat_data > 0].reshape(-1, 1)
-    ).ravel()
+    # scale positive and negative separately
+    scaled_positive = positive_scaler.fit_transform(positive_df)
+    scaled_negative = negative_scaler.fit_transform(negative_df)
 
-    # reshape data back to original shape
-    scaled_df = pd.DataFrame(
-        flat_data.reshape(df.shape), columns=df.columns, index=df.index
-    )
+    df[df > 0] = scaled_positive
+    df[df < 0] = scaled_negative
 
-    return [scaled_df, positive_scaler, negative_scaler]
+    return [df, positive_scaler, negative_scaler]
