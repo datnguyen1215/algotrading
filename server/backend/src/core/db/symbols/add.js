90e479abc163dd1efcb1f8d7ad56adeb7dd3bba2
Dat Nguyen <datnguyen1215@gmail.com>
2024-02-23 04:25:24 -0800

    restructure git directories and candles add() and symbol add().

---
+++ server/backend/src/core/db/symbols/add.js
@@ -0,0 +1,30 @@
+/**
+ * @typedef {object} Symbol
+ * @property {string} name
+ * @property {string} description
+ */
+
+import db from '@src/db';
+import format from 'pg-format';
+
+/**
+ * Add a symbol to the database.
+ * @param {Symbol} symbol
+ */
+const add = async symbol => {
+  // on conflict, update description
+  const query = format(
+    `
+    INSERT INTO symbols (name, description)
+    VALUES (%L, %L)
+    ON CONFLICT (name)
+    DO UPDATE SET description = EXCLUDED.description
+    `,
+    symbol.name,
+    symbol.description
+  );
+
+  return await db.query(query);
+};
+
+export default add;
