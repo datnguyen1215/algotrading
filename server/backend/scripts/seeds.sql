95f73bf4dfd0e2b45e316fb1b6eda26459db9bc7
Dat Nguyen <datnguyen1215@gmail.com>
2024-02-23 04:28:00 -0800

    add files for testings.

---
+++ server/backend/scripts/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO symbols (name, description) VALUES ('SYMBOLTEST', 'Test Symbol');
+
+INSERT INTO candles_m1 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 00:00:00', 'SYMBOLTEST');
+INSERT INTO candles_m1 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 00:01:00', 'SYMBOLTEST');
+
+INSERT INTO candles_m5 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 00:00:00', 'SYMBOLTEST');
+INSERT INTO candles_m5 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 00:05:00', 'SYMBOLTEST');
+
+INSERT INTO candles_m15 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 00:00:00', 'SYMBOLTEST');
+INSERT INTO candles_m15 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 00:15:00', 'SYMBOLTEST');
+
+INSERT INTO candles_m30 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 00:00:00', 'SYMBOLTEST');
+INSERT INTO candles_m30 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 00:30:00', 'SYMBOLTEST');
+
+INSERT INTO candles_h1 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 00:00:00', 'SYMBOLTEST');
+INSERT INTO candles_h1 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 01:00:00', 'SYMBOLTEST');
+
+INSERT INTO candles_h4 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 00:00:00', 'SYMBOLTEST');
+INSERT INTO candles_h4 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 04:00:00', 'SYMBOLTEST');
+
+INSERT INTO candles_d1 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-01 00:00:00', 'SYMBOLTEST');
+INSERT INTO candles_d1 (open, high, low, close, time, symbol) VALUES (1.0, 1.1, 0.9, 1.0, '2021-01-02 00:00:00', 'SYMBOLTEST');
