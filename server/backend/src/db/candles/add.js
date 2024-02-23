e317a5a46a80b1d66938956dcd25db9d238c8f2d
Dat Nguyen <datnguyen1215@gmail.com>
2024-02-23 04:28:53 -0800

    change type.

---
+++ server/backend/src/db/candles/add.js
@@ -0,0 +1,42 @@
+/**
+ * @typedef {object} Candle
+ * @property {string} symbol - the symbol of the asset
+ * @property {number} time - the timestamp of the candle
+ * @property {number} open - the opening price of the candle
+ * @property {number} high - the highest price of the candle
+ * @property {number} low - the lowest price of the candle
+ * @property {number} close - the closing price of the candle
+ */
+
+import db from '@src/db';
+import format from 'pg-format';
+
+/**
+ * Add a candle to the database
+ * @param {Candle[]} candles
+ */
+const add = async candles => {
+  candles[0].created_time;
+  const values = candles.map(candle => [
+    candle.symbol,
+    candle.time,
+    candle.open,
+    candle.high,
+    candle.low,
+    candle.close
+  ]);
+
+  const query = format(
+    `
+    INSERT INTO candles (symbol, time, open, high, low, close)
+    VALUES %L
+    ON CONFLICT DO NOTHING
+    RETURNING *
+    `,
+    values
+  );
+
+  return await db.query(query);
+};
+
+export default add;
