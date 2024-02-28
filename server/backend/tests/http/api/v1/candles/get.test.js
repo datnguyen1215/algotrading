ee64af538c5f735ac32b5816e88f4e792a625d1a
Dat Nguyen <datnguyen1215@gmail.com>
2024-02-28 00:18:15 -0800

    add tests for multi granularity request.

--- server/backend/tests/http/api/v1/candles/get.test.js
+++ server/backend/tests/http/api/v1/candles/get.test.js
@@ -41,35 +41,135 @@ describe('GET /api/v1/candles', () => {
     }
   });
 
-  it('should return 200 with correct data from seeds', async () => {
-    const query = {
-      symbol: 'SYMBOLTEST',
-      interval: 'm1',
-      limit: 10,
-      offset: 0,
-      from: '2021-01-01',
-      to: '2021-01-02'
-    };
-
-    const response = await axios.get(url, { params: query });
-
-    expect(response.status).to.equal(200);
-    expect(response.data.length).to.equal(10);
-  });
-
   it('should return 200 with limit', async () => {
     const query = {
       symbol: 'SYMBOLTEST',
       interval: 'm1',
       limit: 5,
       offset: 0,
-      from: '2021-01-01',
-      to: '2021-01-02'
+      from: '2021-01-01T00:00:00.000Z',
+      to: '2021-01-02T00:00:00.000Z'
     };
 
     const response = await axios.get(url, { params: query });
 
     expect(response.status).to.equal(200);
     expect(response.data.length).to.equal(5);
+  });
+
+  describe('Getting all available granularity data', () => {
+    it('should get m1 data', async () => {
+      const query = {
+        symbol: 'SYMBOLTEST',
+        interval: 'm1',
+        limit: 10,
+        offset: 0,
+        from: '2021-01-01T00:00:00.000Z',
+        to: '2021-01-02T00:00:00.000Z'
+      };
+
+      const response = await axios.get(url, { params: query });
+
+      expect(response.status).to.equal(200);
+      expect(response.data.length).to.equal(10);
+    });
+
+    it('should get m5 data', async () => {
+      const query = {
+        symbol: 'SYMBOLTEST',
+        interval: 'm5',
+        limit: 10,
+        offset: 0,
+        from: '2021-01-01T00:00:00.000Z',
+        to: '2021-01-02T00:00:00.000Z'
+      };
+
+      const response = await axios.get(url, { params: query });
+
+      expect(response.status).to.equal(200);
+      expect(response.data.length).to.equal(10);
+    });
+
+    it('should get m15 data', async () => {
+      const query = {
+        symbol: 'SYMBOLTEST',
+        interval: 'm15',
+        limit: 10,
+        offset: 0,
+        from: '2021-01-01T00:00:00.000Z',
+        to: '2021-01-02T00:00:00.000Z'
+      };
+
+      const response = await axios.get(url, { params: query });
+
+      expect(response.status).to.equal(200);
+      expect(response.data.length).to.equal(10);
+    });
+
+    it('should get m30 data', async () => {
+      const query = {
+        symbol: 'SYMBOLTEST',
+        interval: 'm30',
+        limit: 10,
+        offset: 0,
+        from: '2021-01-01T00:00:00.000Z',
+        to: '2021-01-02T00:00:00.000Z'
+      };
+
+      const response = await axios.get(url, { params: query });
+
+      expect(response.status).to.equal(200);
+      expect(response.data.length).to.equal(10);
+    });
+
+    it('should get h1 data', async () => {
+      const query = {
+        symbol: 'SYMBOLTEST',
+        interval: 'h1',
+        limit: 10,
+        offset: 0,
+        from: '2021-01-01T00:00:00.000Z',
+        to: '2021-01-15T00:00:00.000Z'
+      };
+
+      const response = await axios.get(url, { params: query });
+
+      expect(response.status).to.equal(200);
+      expect(response.data.length).to.equal(10);
+    });
+
+    it('should get h4 data', async () => {
+      const query = {
+        symbol: 'SYMBOLTEST',
+        interval: 'h4',
+        limit: 10,
+        offset: 0,
+        from: '2021-01-01T00:00:00.000Z',
+        to: '2021-01-15T00:00:00.000Z'
+      };
+
+      const response = await axios.get(url, { params: query });
+
+      expect(response.status).to.equal(200);
+      expect(response.data.length).to.equal(10);
+    });
+
+    it('should get d1 data', async () => {
+      const query = {
+        symbol: 'SYMBOLTEST',
+        interval: 'd1',
+        limit: 10,
+        offset: 0,
+        from: '2021-01-01T00:00:00.000Z',
+        to: '2021-01-15T00:00:00.000Z'
+      };
+
+      const response = await axios.get(url, { params: query });
+
+      expect(response.status).to.equal(200);
+      expect(response.data.length).to.equal(10);
+      expect(response.data[0].time).to.equal('2021-01-01T00:00:00.000Z');
+      expect(response.data[9].time).to.equal('2021-01-10T00:00:00.000Z');
+    });
   });
 });
